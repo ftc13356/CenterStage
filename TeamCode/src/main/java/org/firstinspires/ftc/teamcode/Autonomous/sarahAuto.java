ec2a9232641bb7224c5716c522c2ebf24c19af8a
idksarah <sarah.p.ngai@gmail.com>
2024-08-25 14:48:56 -0700

    Add comments, imports, vars, and basic code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/sarahAuto.java
@@ -0,0 +1,36 @@
+/*
+-MotorTest.java
+    -uses composition to access two other utility classes that allows them to use the robot functions, drive functions, and facilitates the packet functions.
+
+- move forward 10 inches and turn 90 clockwise
+    -You will have to make your own utility class. It should contain two functions, moveForward(double inches) and turn(double degrees)
+    -time based
+*/
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@Autonomous()
+public class sarahAuto extends LinearOpMode {
+    private DcMotor backLeft = null;
+    private DcMotor backRight = null;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        backLeft = hardwareMap.get(DcMotor.class, "motorLeftBack");
+        backRight = hardwareMap.get(DcMotor.class, "motorRightBack");
+
+        backLeft.setDirection((DcMotorSimple.Direction.FORWARD));
+        backRight.setDirection((DcMotorSimple.Direction.REVERSE));
+
+        waitForStart();
+
+        backLeft.setPower(0);
+        backRight.setPower(0);
+        sleep(2000); //random time tbh
+
+    }
+}
