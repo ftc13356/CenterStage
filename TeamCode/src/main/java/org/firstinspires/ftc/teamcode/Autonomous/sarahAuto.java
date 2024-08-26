31de3eb2b2055d8c216c2e9dd4da73a46a5cf744
idksarah <sarah.p.ngai@gmail.com>
2024-08-25 17:14:01 -0700

    Integrate sarahAuto and sarahAutoUtility

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/sarahAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/sarahAuto.java
@@ -6,24 +6,18 @@
     -You will have to make your own utility class. It should contain two functions, moveForward(double inches) and turn(double degrees)
     -time based
 */
+
 package org.firstinspires.ftc.teamcode.Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-
-import org.firstinspires.ftc.teamcode.Autonomous.sarahAutoUtility;
 
 @Autonomous()
 public class sarahAuto extends LinearOpMode {
-    private DcMotor backLeft = null;
-    private DcMotor backRight = null;
-
     @Override
     public void runOpMode() throws InterruptedException {
-        backLeft = hardwareMap.get(DcMotor.class, "motorLeftBack");
-        backRight = hardwareMap.get(DcMotor.class, "motorRightBack");
-
+        sarahAutoUtility fatty = new sarahAutoUtility();
+        fatty.moveForward(10);
+        fatty.turn(90);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/sarahAutoUtility.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/sarahAutoUtility.java
@@ -1,26 +1,32 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap; //idk what this is
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class sarahAutoUtility {
-    public DcMotor backLeft, backRight;
+    public DcMotor backLeft = hardwareMap.get(DcMotor.class, "motorLeftBack");
+    public DcMotor backRight = hardwareMap.get(DcMotor.class, "motorRightBack");
 
-    public void moveForward(double distance){
-        //currently doesn't do anything with distance but will do something with it with encoders later
-        ElapsedTime elapsedTime = new ElapsedTime(); //timer
+    public void moveForward(double distance){ //timer
+        ElapsedTime elapsedTime = new ElapsedTime();
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backRight.setDirection(DcMotorSimple.Direction.FORWARD);
         setPower(1,1);
-        if(elapsedTime.time() > 2.5){
+        while(elapsedTime.time() < 2.5){ //timer
             setPower(0,0);
         }
     }
 
     public void turn(double degrees){
+        ElapsedTime elapsedTime = new ElapsedTime();
         backLeft.setDirection(DcMotorSimple.Direction.FORWARD); //leftmotor rotates forward and rightmotor rotates backward
         setPower(.5,.5);
+        while(elapsedTime.time() < 3){ //timer
+            setPower(0,0);
+        }
     }
 
     public void setPower(double powerLeft, double powerRight){
