cfc1fa9c19edf9aae834ee03a4e5714bad9207c0
idksarah <sarah.p.ngai@gmail.com>
2024-09-02 12:12:56 -0700

    Update sarahAutoUtilityTimer.java
    tbh idek how i managed to break things this bad its kind of impressive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/sarahAutoUtilityTimer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/sarahAutoUtilityTimer.java
@@ -1,42 +1,39 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.Components.RFModules.Devices.Line;
+
 public class sarahAutoUtilityTimer {
     public DcMotor backLeft;
     public DcMotor backRight;
     private HardwareMap hardwareMap;
+    public LinearOpMode op;
 
-    public sarahAutoUtilityTimer(HardwareMap hwMap) {
-        hardwareMap = hwMap;
+    public sarahAutoUtilityTimer(LinearOpMode opMode) {
+        op = opMode;
+        hardwareMap = op.hardwareMap;
         backLeft = hardwareMap.get(DcMotor.class, "motorLeftBack");
         backRight = hardwareMap.get(DcMotor.class, "motorRightBack");
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
     public void moveForward(double distance){
-        ElapsedTime elapsedTime = new ElapsedTime();
-        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        setPower(1,1);
-        elapsedTime.reset();
-        while(elapsedTime.time() < 2.5){
-        }
+        setPower(.4,.4);
+        op.sleep(5000);
         setPower(0,0);
     }
 
     public void turn(double degrees){
-        ElapsedTime elapsedTime = new ElapsedTime();
-        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        setPower(.5,.5);
-        elapsedTime.reset();
-        while(elapsedTime.time() < 3){
-        }
+        setPower(-.15,.15);
+        op.sleep(5000);
         setPower(0,0);
     }
 
