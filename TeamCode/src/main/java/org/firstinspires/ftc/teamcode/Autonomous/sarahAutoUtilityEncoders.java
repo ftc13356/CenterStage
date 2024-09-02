5fb9aac3e44532b9239bb0148c54bf1422ba1715
idksarah <sarah.p.ngai@gmail.com>
2024-09-02 12:10:50 -0700

    Update sarahAutoUtilityEncoders.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/sarahAutoUtilityEncoders.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/sarahAutoUtilityEncoders.java
@@ -1,41 +1,76 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-public class sarahAutoUtilityEncoders{
+public class sarahAutoUtilityEncoder{
     public DcMotor backLeft;
     public DcMotor backRight;
     private HardwareMap hardwareMap;
-    public int ticks = 200; //too lazy to get the real ones rn
-    //initialized backRight and backLeft as encoder thingise??? code doesn't actualyl do anything with it tho
+    public double ticks;
+    public LinearOpMode opMode;
+    private ElapsedTime runtime = new ElapsedTime();
 
-    public sarahAutoUtilityEncoders(HardwareMap hwMap) {
-        hardwareMap = hwMap;
+    //put this gross ass code into some methods pls god
+    public sarahAutoUtilityEncoder(LinearOpMode op) {
+        hardwareMap = op.hardwareMap;
+        opMode = op;
         backLeft = hardwareMap.get(DcMotor.class, "motorLeftBack");
-        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        setDirectionAndMode(backLeft, "reverse", "usingEncoder");
 
         backRight = hardwareMap.get(DcMotor.class, "motorRightBack");
-        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        setDirectionAndMode(backLeft, "forward", "usingEncoder");
     }
     public void moveForward(double distance){
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        setPower(1,1);
-    }
-
-    public void turn(double degrees){
-        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        setPower(.5,.5);
+        ticks = getTicks(384.5, distance, 2);
+        backLeft.setTargetPosition((int)ticks);
+        backRight.setTargetPosition((int)ticks);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        setPower(.4,.4);
+        while (opMode.opModeIsActive() &&
+                (runtime.seconds() < 3) && //pulled this numebr otu of myass
+                (backLeft.isBusy() && backRight.isBusy())) {
+            opMode.sleep(50);
+        }
         setPower(0,0);
     }
-
+    public void turn(double degrees){ //naur
+        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        backLeft.setTargetPosition((int)384.5/2); //idec naymore ong bru
+        backRight.setTargetPosition((int)384.5/2);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        setPower(.15,.15);
+    }
+    public void setDirectionAndMode (DcMotor motor, String direction, String mode){
+        if(direction == "forward"){
+            motor.setDirection(DcMotorSimple.Direction.FORWARD);
+        } else {
+            motor.setDirection(DcMotorSimple.Direction.REVERSE);
+        }
+        if(mode == "usingEncoder"){
+            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        } else if(mode =="toPosition") {
+            motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+    }
     public void setPower(double powerLeft, double powerRight){
         backLeft.setPower(powerLeft);
         backRight.setPower(powerRight);
+    }
+    public double getTicks(double ticks, double distance, double wheelRadius) {
+        if(ticks == 0.0) {
+            ticks = 384.5;
+        }
+        double circum = 2 * wheelRadius * 3.14159;
+        double turns = 10/circum;
+        return turns * ticks; //help i can't do math
     }
 }
