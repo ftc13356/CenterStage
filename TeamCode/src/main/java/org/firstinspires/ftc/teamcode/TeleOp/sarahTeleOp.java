b9678c5c0e88cb936a2a8041ca34a4d14b1ab883
idksarah <sarah.p.ngai@gmail.com>
2024-09-07 00:09:27 -0700

    add claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/sarahTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/sarahTeleOp.java
@@ -4,16 +4,19 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp()
 public class sarahTeleOp extends LinearOpMode {
     private DcMotor backRight = null;
     private DcMotor backLeft = null;
+    private Servo claw = null; //idk the class but whatever
 
     @Override
     public void runOpMode() throws InterruptedException {
         backLeft = hardwareMap.get(DcMotor.class, "motorLeftBack");
         backRight = hardwareMap.get(DcMotor.class, "motorRightBack");
+        claw = hardwareMap.get(Servo.class, "servoFront");
 
         backRight.setDirection(DcMotorSimple.Direction.FORWARD);
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -26,8 +29,14 @@ public class sarahTeleOp extends LinearOpMode {
             telemetry.addData("drive", Double.toString(drive));
             telemetry.addData("turn", Double.toString(turn));
 
-            backLeft.setPower(drive-turn);
-            backRight.setPower(drive+turn);
+            backLeft.setPower(drive+turn);
+            backRight.setPower(drive-turn);
+
+            if(gamepad1.a) {
+                claw.setPosition(1);
+            } else {
+                claw.setPosition(0);
+            }
         }
 
     }
