df5047a07e3f2b06ed334e2aa79e24c42f7494b3
idksarah <sarah.p.ngai@gmail.com>
2024-09-07 00:09:17 -0700

    adjust sleep() durations

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/sarahAutoUtilityTimer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/sarahAutoUtilityTimer.java
@@ -25,14 +25,14 @@ public class sarahAutoUtilityTimer {
     public void moveForward(double distance){
         backLeft.setPower(.4);
         backRight.setPower(.4);
-        op.sleep(500);
+        op.sleep(700);
         backLeft.setPower(0);
         backRight.setPower(0);
     }
 
     public void turn(double degrees){
         backLeft.setPower(.4);
-        op.sleep(800);
+        op.sleep(1500);
         backLeft.setPower(0);
     }
 }
