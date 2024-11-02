c5779cd4ef605f0c52690b296f58795145a8078a
idksarah <sarah.p.ngai@gmail.com>
2024-11-02 16:19:24 -0700

    reformat and idk it dosnt rlly matter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/leftRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/leftRight.java
@@ -1,12 +1,12 @@
-package org.firstinspires.ftc.teamcode.Autonomous;
+/*package org.firstinspires.ftc.teamcode.Autonomous;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.pedroPathing.follower.Follower;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierCurve;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Path;
@@ -27,47 +27,44 @@ public class leftRight extends OpMode {
 
     private Pose endPose;
 
-    @Override
-    public void init(){
+    /*public void init(){
         follower = new Follower(hardwareMap);
-
-        switch (direction){
-            default:
-            case "left":
-                endPose = new Pose(distance, distance, Point.CARTESIAN);
-                break;
-            case "right":
-                endPose = new Pose(-distance, distance, Point.CARTESIAN);
-                break;
+        public void setBackdropGoalPose() {
+            switch (direction){
+                default:
+                case "left":
+                    endPose = new Pose(distance, distance, Point.CARTESIAN);
+                    firstLeft = follower.pathBuilder()
+                            .addPath(new BezierCurve(new Point(0,0, Point.CARTESIAN), new Point(0, distance, Point.CARTESIAN), new Point(endPose.getX(), endPose.getY(), Point.CARTESIAN)))
+                            .build();
+                    follower.followPath(firstLeft);
+                    break;
+                case "right":
+                    endPose = new Pose(-distance, distance, Point.CARTESIAN);
+                    firstRight = follower.pathBuilder()
+                            .addPath(new BezierCurve(new Point(endPose.getX(), endPose.getY(), Point.CARTESIAN), new Point(0, distance, Point.CARTESIAN), new Point(0,0,Point.CARTESIAN)))
+                            .build();
+                    follower.followPath(firstRight);
+                    break;
+            }
         }
 
-        firstLeft = follower.pathBuilder()
-                .addPath(new BezierCurve(new Point(0,0, Point.CARTESIAN), new Point(0, distance, Point.CARTESIAN), new Point(endPose.getX(), endPose.getY(), Point.CARTESIAN)))
-                .build();
 
         follower.followPath(firstLeft);
 
-        firstRight = follower.pathBuilder()
-                .addPath(new BezierCurve(new Point(endPose.getX(), endPose.getY(), Point.CARTESIAN), new Point(0, distance, Point.CARTESIAN), new Point(0,0,Point.CARTESIAN)))
-                .build();
     }
 
+
+    @Override
+    public void start() {
+        //setBackdropGoalPose();
+        //buildPaths();
+    }
+
+    @Override
     public void loop() {
         follower.update();
-        if(!follower.isBusy()){
-            switch(direction) {
-                default:
-                case "firstLeft":
-                    direction = "firstRight";
-                    follower.followPath(firstRight);
-                    break;
-                case "firstRight":
-                    direction = "firstLeft";
-                    follower.followPath(firstLeft);
-                }
-            }
-        telemetryA.addData("direction:", direction);
-        follower.telemetryDebug(telemetryA);
-        }
     }
+
+}*/
 
