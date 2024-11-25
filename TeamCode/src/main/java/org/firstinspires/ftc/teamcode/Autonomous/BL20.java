0b506da804a03eaff7ed51697fd930d3e6aa4780
idksarah <sarah.p.ngai@gmail.com>
2024-11-24 17:01:04 -0800

    idk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BL20.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BL20.java
@@ -3,7 +3,9 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.Components.Claw;
+import org.firstinspires.ftc.teamcode.Components.Flip;
 import org.firstinspires.ftc.teamcode.Components.TelescopicArm;
+import org.firstinspires.ftc.teamcode.Components.Twist;
 import org.firstinspires.ftc.teamcode.Robots.IDRobot;
 import org.firstinspires.ftc.teamcode.pedroPathing.localization.PoseUpdater;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierCurve;
@@ -14,8 +16,8 @@ import org.firstinspires.ftc.teamcode.pedroPathing.localization.Pose;
 
 public class BL20 {
     PoseUpdater poseUpdater;
-    Follower follower;
-    PathChain scoreSampley, getSampley1, getSampley2, submersibley;
+    Follower builder;
+    PathChain scoreSample, yellow1, yellow2, submersible;
     IDRobot robot;
 
     Pose starting = new Pose(12,108,0);
@@ -23,20 +25,20 @@ public class BL20 {
     public BL20(LinearOpMode opmode){
         robot = new IDRobot(opmode,false);
 
-        follower = new Follower(opmode.hardwareMap);
-        follower.setStartingPose(starting);
+        builder = new Follower(opmode.hardwareMap);
+        builder.setStartingPose(starting);
 
-        getSampley1 = follower.pathBuilder()
+        yellow1 = builder.pathBuilder()
                 .addPath(new BezierCurve(new Point(12,108,Point.CARTESIAN), new Point(24,120,0)))
                 .setLinearHeadingInterpolation(0,0)
                 .build();
 
-        getSampley2 = follower.pathBuilder()
+        yellow2 = builder.pathBuilder()
                 .addPath(new BezierCurve(new Point(20,124,Point.CARTESIAN), new Point(24,130,Point.CARTESIAN)))
                 .setLinearHeadingInterpolation(Math.PI*3/4, 0)
                 .build();
 
-        submersibley = follower.pathBuilder()
+        submersible = builder.pathBuilder()
                 .addPath(new BezierCurve(new Point(20,124,Point.CARTESIAN), new Point(60,110,Point.CARTESIAN), new Point(60,96,Point.CARTESIAN)))
                 .setLinearHeadingInterpolation(Math.PI*3/4, -Math.PI/2)
                 .build();
@@ -45,48 +47,46 @@ public class BL20 {
         robot.setClaw(Claw.ClawStates.CLOSED, false);
     }
     /**
-    * builds + follows path to go from current point -> sample-scoring
+    * builds + follows path to go from current point -> (20,124)
      */
     public void placeSample () {
-        Pose current = follower.getPose();
-        scoreSampley = follower.pathBuilder()
+        Pose current = builder.getPose();
+        scoreSample = builder.pathBuilder()
                 .addPath(new BezierCurve(new Point(current.getX(), current.getY(), Point.CARTESIAN), new Point(20,124,Point.CARTESIAN)))
                 .setLinearHeadingInterpolation(0, Math.PI*3/4)
                 .build();
-        robot.followPath(scoreSampley, true);
+        robot.followPath(scoreSample, true);
     }
 
-    /**
-     * follows paths + sets claw and arm to pick up and places yellow samples 1,2
-     */
-
-    public void afterGetSampley(){
+    public void afterYellow(){
         robot.setClaw(Claw.ClawStates.OPEN, true);
         robot.setArm(TelescopicArm.ArmStates.INTAKE, true);
+        robot.setTwist(Twist.TwistStates.GRAB, true);
+        robot.setFlip(Flip.FlipStates.SPECIMEN_GRAB, true);
         robot.queuer.addDelay(3.0);
+        robot.setClaw(Claw.ClawStates.CLOSED, false);
 
         placeSample();
-        robot.setClaw(Claw.ClawStates.CLOSED, true);
         robot.setArm(TelescopicArm.ArmStates.HIGH_BUCKET, true);
+        robot.setTwist(Twist.TwistStates.PERPENDICULAR, true);
+        robot.setFlip(Flip.FlipStates.SPECIMEN, true);
         robot.queuer.addDelay(3.0);
+        robot.setClaw(Claw.ClawStates.OPEN, false);
     }
 
     public void placeSamples(){
-        robot.followPath(getSampley1, true);
-        afterGetSampley();
-        robot.followPath(getSampley2,true);
-        afterGetSampley();
+        robot.followPath(yellow1, true);
+        afterYellow();
+        robot.followPath(yellow2,true);
+        afterYellow();
     }
 
-    /**
-     * parks by submersible
-     */
     public void park() {
-        robot.followPath(submersibley,true);
+        robot.followPath(submersible,true);
     }
 
     public void updateFollower() {
-        follower.update();
+        builder.update();
         robot.queuer.setFirstLoop(false);
 
         poseUpdater.update();
