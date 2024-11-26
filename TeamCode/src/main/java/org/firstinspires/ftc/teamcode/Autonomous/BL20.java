95ab1fdaaa3147460c38f85a453fadcc2a2d25a1
idksarah <sarah.p.ngai@gmail.com>
2024-11-26 14:05:23 -0800

    use preloaded sample

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BL20.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BL20.java
@@ -7,7 +7,6 @@ import org.firstinspires.ftc.teamcode.Components.Flip;
 import org.firstinspires.ftc.teamcode.Components.TelescopicArm;
 import org.firstinspires.ftc.teamcode.Components.Twist;
 import org.firstinspires.ftc.teamcode.Robots.IDRobot;
-import org.firstinspires.ftc.teamcode.pedroPathing.localization.PoseUpdater;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierCurve;
 import org.firstinspires.ftc.teamcode.pedroPathing.follower.Follower;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.PathChain;
@@ -15,12 +14,9 @@ import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Point;
 import org.firstinspires.ftc.teamcode.pedroPathing.localization.Pose;
 
 public class BL20 {
-    //Follower builder;
-    PathChain scoreSample, yellow1, yellow2, submersible;
+    PathChain scoreSample, yellow1, submersible;
     IDRobot robot;
-
     Pose starting = new Pose(12,108,0);
-
     public BL20(LinearOpMode opmode){
         robot = new IDRobot(opmode,false);
 
@@ -28,13 +24,8 @@ public class BL20 {
         robot.follower.setStartingPose(starting);
 
         yellow1 = robot.follower.pathBuilder()
-                .addPath(new BezierCurve(new Point(12,108,Point.CARTESIAN), new Point(24,120,0)))
-                .setLinearHeadingInterpolation(0,0)
-                .build();
-
-        yellow2 = robot.follower.pathBuilder()
-                .addPath(new BezierCurve(new Point(20,124,Point.CARTESIAN), new Point(24,130,Point.CARTESIAN)))
-                .setLinearHeadingInterpolation(Math.PI*3/4, 0)
+                .addPath(new BezierCurve(new Point(20,124,Point.CARTESIAN), new Point(24,120,0)))
+                .setLinearHeadingInterpolation(Math.PI*3/4,0)
                 .build();
 
         submersible = robot.follower.pathBuilder()
@@ -55,6 +46,11 @@ public class BL20 {
                 .setLinearHeadingInterpolation(0, Math.PI*3/4)
                 .build();
         robot.followPath(scoreSample, true);
+
+        robot.setArm(TelescopicArm.ArmStates.HIGH_BUCKET, true);
+        robot.setTwist(Twist.TwistStates.PERPENDICULAR, true);
+        robot.setFlip(Flip.FlipStates.SPECIMEN, true);
+        robot.setClaw(Claw.ClawStates.OPEN, false);
     }
 
     public void afterYellow(){
@@ -62,21 +58,13 @@ public class BL20 {
         robot.setArm(TelescopicArm.ArmStates.INTAKE, true);
         robot.setTwist(Twist.TwistStates.GRAB, true);
         robot.setFlip(Flip.FlipStates.SPECIMEN_GRAB, true);
-        robot.queuer.addDelay(3.0);
         robot.setClaw(Claw.ClawStates.CLOSED, false);
-
         placeSample();
-        robot.setArm(TelescopicArm.ArmStates.HIGH_BUCKET, true);
-        robot.setTwist(Twist.TwistStates.PERPENDICULAR, true);
-        robot.setFlip(Flip.FlipStates.SPECIMEN, true);
-        robot.queuer.addDelay(3.0);
-        robot.setClaw(Claw.ClawStates.OPEN, false);
     }
 
     public void placeSamples(){
+        placeSample();
         robot.followPath(yellow1, true);
-        afterYellow();
-        robot.followPath(yellow2,true);
         afterYellow();
     }
 
@@ -86,7 +74,6 @@ public class BL20 {
 
     public void updateFollower() {
         robot.queuer.setFirstLoop(false);
-
         robot.update();
     }
 }
