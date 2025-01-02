4ad76a66c6d636aff84f2cd85de692545b0d9d18
WarZ43 <minecraftplayer6542@gmail.com>
2025-01-02 13:48:38 -0800

    slide

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/RFModules/Devices/DualPIDController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/RFModules/Devices/DualPIDController.java
@@ -52,7 +52,7 @@ public class DualPIDController {
         vel =0;
         rP = 0.012; rP2 =0.02;rD2= 2;
         rD = 0.6; rG = 0.16;
-        rG2 = 0.7;
+        rG2 = 0.6;
         if(!voltScaled) {
             rP*= 13 / voltage;
             rP2*= 13 / voltage;
