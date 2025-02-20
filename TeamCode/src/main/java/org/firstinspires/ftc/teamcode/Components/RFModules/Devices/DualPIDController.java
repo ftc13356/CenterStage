70ba8081f4af731ceff1b3116ebea1159721ed52
idksarah <sarah.p.ngai@gmail.com>
2025-02-19 16:00:18 -0800

    sorry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/RFModules/Devices/DualPIDController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/RFModules/Devices/DualPIDController.java
@@ -27,9 +27,14 @@ import org.firstinspires.ftc.teamcode.Components.TelescopicArm;
 public class DualPIDController {
     public static double x1 = 0;
     DcMotorEx ext, ext2, rot, extEnc, rotEnc;
+//    public static double  A_OFF = -15, MAX=31.5, MIN=0
+//            , ROTMAX = 162, ROTMIN = 0, TICKS_PER_IN = 0.001821464277011343*4*31/79*30/35, TICKS_PER_DEG = 380/8192.0,P=0.2,D=0.02, rP = 0.025 , rP2 =0.03, rD2= 2
+//            , rD = .25 , rF = 0.4, G = 0.3,rG = 0.2, rG2 = 0.35, HORIZ_LIM = 27.2
+//            ,TEST_LEN = 0, MAX_SPEED = 223*751.8/60, MULT = -1, MULT2=-1;
+
     public static double  A_OFF = -15, MAX=31.5, MIN=0
-            , ROTMAX = 162, ROTMIN = 0, TICKS_PER_IN = 0.001821464277011343*4*31/79*30/35, TICKS_PER_DEG = 380/8192.0,P=0.2,D=0.02, rP = 0.025 , rP2 =0.03, rD2= 2
-            , rD = .25 , rF = 0.4, G = 0.3,rG = 0.2, rG2 = 0.35, HORIZ_LIM = 27.2
+            , ROTMAX = 162, ROTMIN = 0, TICKS_PER_IN = 0.001821464277011343*4*31/79*30/35, TICKS_PER_DEG = 380/8192.0,P=0.2,D=0.02, rP = 0.016 , rP2 =0.015, rD2= .05
+            , rD = .01 , rF = 0.3, G = 0.3,rG = 0.225, rG2 = 0.5, HORIZ_LIM = 27.2
             ,TEST_LEN = 0, MAX_SPEED = 223*751.8/60, MULT = -1, MULT2=-1;
     boolean mid=true, voltScaled = false;
     double TICKS_PER_RAD = TICKS_PER_DEG*PI/180;
